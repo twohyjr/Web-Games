7d1821afcffb7ad052d1d5fed1abe163cf9f88d8
Rick Twohy <twohyjr@gmail.com>
2017-01-24 21:57:45 -0600

    tetris is now drawing

--- app/components/games/2e-tetris.js
+++ app/components/games/2e-tetris.js
@@ -1,11 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+     screenWidth: 400,
+     screenHeight: 800,
+     cellSize: 50,
+
      actions: {
           startTetris(){
-               //Start the game here
+               let canvas = document.getElementById('tetrisCanvas');
+               let ctx = canvas.getContext("2d");
+
+               ctx.fillStyle = 'red';
+               ctx.beginPath();
+               ctx.fillRect(
+                         (this.get('screenWidth')/2) - (this.get('cellSize')/2),
+                         (this.get('screenHeight')/2) - (this.get('cellSize')/2),
+                         this.get('cellSize'),
+                         this.get('cellSize')
+                    );
+               ctx.closePath();
+               ctx.fill();
           }
-     }
+     },
+
 
 
 });
